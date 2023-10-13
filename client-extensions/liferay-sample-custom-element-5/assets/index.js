c27bb657476d5867284f2e5693ad577a8cd3c0c7
Nathan Ferracini <nathan.ferracini@bp.liferay.com>
2023-10-13 15:34:16 -0300

    Add client-extension from com.liferay.sample.workspace-latest.zip

---
+++ client-extensions/liferay-sample-custom-element-5/assets/index.js
@@ -0,0 +1,38 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayBadge from '@clayui/badge';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomElement extends HTMLElement {
+	constructor() {
+		super();
+
+		const root = document.createElement('div');
+
+		this.appendChild(root);
+
+		ReactDOM.render(
+			React.createElement(ClayBadge, {
+				displayType: 'success',
+				label: 'Success!',
+			}),
+			root
+		);
+	}
+}
+
+const ELEMENT_NAME = 'liferay-sample-custom-element-5';
+
+if (customElements.get(ELEMENT_NAME)) {
+	// eslint-disable-next-line no-console
+	console.log(
+		`Skipping registration for <${ELEMENT_NAME}> (already registered)`
+	);
+}
+else {
+	customElements.define(ELEMENT_NAME, CustomElement);
+}
